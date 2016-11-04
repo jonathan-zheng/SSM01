e4b7470fc2114ab713bee49aa649b80d58bc4c91
jonathan <jonathanzheng@aaxischina.com>
2016-11-04 17:16:04 +0800

    02

---
+++ SSM/src/main/java/com/zheng/service/UserService.java
@@ -0,0 +1,7 @@
+package com.zheng.service;
+
+import com.zheng.pojo.User;
+
+public interface  UserService {
+	public User getUserById(int userId);  
+}
--- SSM/src/main/java/com/zheng/service/impl/IUserServiceImpl.java
+++
@@ -1,5 +0,0 @@
-package com.zheng.service.impl;
-
-public class IUserServiceImpl {
-
-}
